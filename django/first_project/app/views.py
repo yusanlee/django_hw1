08d7d2d1c6b958025937cf2ba49d1e122501afb2
yusanlee <154800004+yusanlee@users.noreply.github.com>
2023-12-25 13:32:17 +0400

    django_hw1

---
+++ django/first_project/app/views.py
@@ -0,0 +1,30 @@
+import datetime
+import os
+
+from django.http import HttpResponse
+from django.shortcuts import render, reverse
+
+
+def home_view(request):
+    template_name = 'app/home.html'
+    pages = {
+        'Главная страница': reverse('home'),
+        'Показать текущее время': reverse('time'),
+        'Показать содержимое рабочей директории': reverse('workdir')
+    }
+
+    context = {
+        'pages': pages
+    }
+    return render(request, template_name, context)
+
+
+def time_view(request):
+    current_time = datetime.datetime.now()
+    msg = f'Текущее время: {current_time}'
+    return HttpResponse(msg)
+
+
+def workdir_view(request):
+    dirs = os.listdir()
+    return HttpResponse('<br>'.join(dirs))
